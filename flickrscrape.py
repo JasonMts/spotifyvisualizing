f9413321b5627357cd01a85135f2a31484bb2f1b
JasonMts <iasonasm@gmail.com>
2017-05-13 19:52:07 -0700

    Added rank by repetition function

--- flickrscrape.py
+++ flickrscrape.py
@@ -2,13 +2,14 @@ from flickrapi import FlickrAPI
 from pprint import pprint
 from urllib.request import urlretrieve
 
+#keys
 FLICKR_PUBLIC = '0ba5a62e543bd1a4b2c8f662a16577ac'
 FLICKR_SECRET = '74261c8a79410e10'
 
-
+#function that gets pictures given a list of words
 def getpictures(keywords):
     counter = 0
-    # counter = 0
+
     for word in keywords:
         flickr = FlickrAPI(FLICKR_PUBLIC, FLICKR_SECRET, format='parsed-json')
         extras='url_c, url_m'
@@ -23,7 +24,22 @@ def getpictures(keywords):
         counter += 1
         urlretrieve(pic, "pic" + str(counter) + ".jpg")
 
-
 # def main():
-list = ['yellow', 'blue', 'black']
-getpictures(list)
+
+def getbestwords(filename):
+    f = open(filename, 'r')
+    idx = {}
+    contents = f.read().lower()
+    print(contents)
+
+    for word in contents.split():
+        if word in idx:
+            idx[word] += 1
+        else:
+            idx[word] = 1
+
+    print(idx)
+
+#list = ['yellow', 'blue', 'black']
+#getpictures(list)
+getbestwords("file.txt")
