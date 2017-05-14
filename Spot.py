43a049a986b317fa76225ee63f5774f345a879fa
Akatzuka <Svucinich@csumb.edu>
2017-05-14 16:53:07 -0700

    Wrong file committed

--- Spot.py
+++ Spot.py
@@ -60,9 +60,9 @@ def play_spot():
     #         pay.append(json.loads(line))
     #arr = json.loads(data)
     #payload = data
-    r = requests.put('https://api.spotify.com/v1/me/player/play', data = {'access_token': 'BQBq_08n-82eVI1hKUJo5rHRkz9GfHjHpdpKao6u71O6x7-91tnFZH2Bc_Kx9nXfPXQZ4GCJ3lOnSPyYP3gmVbHFDTYxeGvn49jcTeusm2Npst_xre8aq1gEhqOeGYErINgZutZ3Eot9mEYo6w'})
-    print(r.status_code)
-    print(r.content)
+    #r = requests.put('https://api.spotify.com/v1/me/player/play', data = {'access_token': })
+    #print(r.status_code)
+    #print(r.content)
 
 def pause_spot():
     scope = 'user-modify-playback-state'
@@ -83,47 +83,49 @@ def pause_spot():
     #         pay.append(json.loads(line))
     #arr = json.loads(data)
     #payload = data
-    r = requests.put('https://api.spotify.com/v1/me/player/pause', data = {'access_token': 'BQBq_08n-82eVI1hKUJo5rHRkz9GfHjHpdpKao6u71O6x7-91tnFZH2Bc_Kx9nXfPXQZ4GCJ3lOnSPyYP3gmVbHFDTYxeGvn49jcTeusm2Npst_xre8aq1gEhqOeGYErINgZutZ3Eot9mEYo6w'})
-    print(r.status_code)
-    print(r.content)
+    #r = requests.put('https://api.spotify.com/v1/me/player/pause', data = {'access_token': })
+    #print(r.status_code)
+    #print(r.content)
 
-#Cached token check, otherwise generate new token
-a = SpotifyOAuth.get_cached_token
-if (a != None):
-    c_token = SpotifyOAuth.get_cached_token
-else:
-    c_token = util.prompt_for_user_token(username, scope, SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI)
+def playlist_get():
+    #Cached token check, otherwise generate new token
+    a = SpotifyOAuth.get_cached_token
+    if (a != None):
+        c_token = SpotifyOAuth.get_cached_token
+    else:
+        c_token = util.prompt_for_user_token(username, scope, SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI)
 
-#if token exists print all user playlists
-if token:
-    playlists = sp.user_playlists(username)
-    p_num = 1
-    for playlist in playlists['items']:
-            print("  %d %s" % (p_num, playlist['name']))
-            p_num = p_num + 1
-    #allow user to choose which playlist to print
-    playlist_choice = input()
+    #if token exists print all user playlists
+    if token:
+        playlists = sp.user_playlists(username)
+        p_num = 1
+        for playlist in playlists['items']:
+                print("  %d %s" % (p_num, playlist['name']))
+                p_num = p_num + 1
+        #allow user to choose which playlist to print
+        playlist_choice = input()
 
-    if (int(playlist_choice) > (p_num - 1) | int(playlist_choice) < (p_num - 1)):
-        print("Playlist number is not applicable.")
-        sys.exit()
+        if (int(playlist_choice) > (p_num - 1) | int(playlist_choice) < (p_num - 1)):
+            print("Playlist number is not applicable.")
+            sys.exit()
 
-    #playlist print
-    count = 1
-    for playlist in playlists['items']:
-        if (int(playlist_choice) == count and playlist['owner']['id'] == username):
-            print()
-            print(playlist['name'])
-            print('  total tracks', playlist['tracks']['total'])
-            results = sp.user_playlist(username, playlist['id'], fields="tracks,next")
-            tracks = results['tracks']
-            show_tracks(tracks)
-            count = count + 1
-            while tracks['next']:
-                tracks = sp.next(tracks)
-                show_tracks(tracks)
-        else:
-            count = count + 1
-            continue
-else:
-    print("Can't get token for", username)
+        #playlist print
+        count = 1
+        for playlist in playlists['items']:
+            if (int(playlist_choice) == count and playlist['owner']['id'] == username):
+                print()
+                print(playlist['name'])
+                print('  total tracks', playlist['tracks']['total'])
+                #results = sp.user_playlist(username, playlist['id'], fields="tracks,next")
+                #tracks = results['tracks']
+                # show_tracks(tracks)
+                count = count + 1
+                # while tracks['next']:
+                #     tracks = sp.next(tracks)
+                #     show_tracks(tracks)
+            else:
+                count = count + 1
+                continue
+    else:
+        print("Can't get token for", username)
+playlist_get()
