ec8e456f5312f205aa789cfae8ef1d21810e3ce2
JasonMts <iasonasm@gmail.com>
2017-05-14 02:50:09 -0700

    Extra checks and made 3 functions

--- flickrscrape.py
+++ flickrscrape.py
@@ -1,6 +1,9 @@
+#library imports
 from flickrapi import FlickrAPI
 from pprint import pprint
 from urllib.request import urlretrieve
+from collections import Counter
+from nltk.corpus import wordnet as wn
 
 #keys
 FLICKR_PUBLIC = '0ba5a62e543bd1a4b2c8f662a16577ac'
@@ -10,36 +13,69 @@ FLICKR_SECRET = '74261c8a79410e10'
 def getpictures(keywords):
     counter = 0
 
+    #Make API call and get results in JSON format
     for word in keywords:
         flickr = FlickrAPI(FLICKR_PUBLIC, FLICKR_SECRET, format='parsed-json')
         extras='url_c, url_m'
         results = flickr.photos.search(text=word, per_page=6, extras=extras)
 
-        photos = results['photos'] #get the photos information
+        #get the photos information from the JSON
+        photos = results['photos']
         pprint(photos['photo'][0]['url_m'])
+
+        #Keep just the picture URL
         pic = photos['photo'][0]['url_m']
-# print(pic)
-# pprint(photos['photo'][1]['url_m'])
-# pprint(photos['photo'][2]['url_m'])
+
+        #Save the picture
         counter += 1
         urlretrieve(pic, "pic" + str(counter) + ".jpg")
 
-# def main():
 
 def getbestwords(filename):
+    #open file passed as argument
     f = open(filename, 'r')
     idx = {}
-    contents = f.read().lower()
-    print(contents)
+    top = []
 
+    #Read lyrics, convert them to lowercase and take out apostrophes
+    contents = f.read().lower().replace("'", "")
+
+    #create a dict with word frequency
     for word in contents.split():
-        if word in idx:
+        if word in idx and len(word) > 4:
             idx[word] += 1
         else:
             idx[word] = 1
 
-    print(idx)
+    #use Counter library to find the most common words
+    countable = Counter(idx)
+    mostcommon = countable.most_common(10)
+
+    #get just the words
+    for i in range(len(mostcommon)):
+        top.append(mostcommon[i][0])
+
+    return(top)
+
+
+#This function checks if the top words are nouns returns them
+def checknouns(top):
+    allnouns = []
+    search = []
+
+    #get list of nouns from database
+    for i in wn.all_synsets('n'):
+        allnouns.append(i.name().split('.',1)[0])
+
+    #check if the words are noun and print them out if they are
+    for j in top:
+        if j in allnouns:
+            search.append(j)
+
+    #search results
+    print(search)
+    return(search)
 
 #list = ['yellow', 'blue', 'black']
 #getpictures(list)
-getbestwords("file.txt")
+getpictures(checknouns(getbestwords("file.txt")))
