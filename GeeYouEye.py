aac51dab536264f6a2c5c2e2361e575701966bd7
Your Name <cmarchenko@csumb.edu>
2017-05-14 18:28:47 -0700

    Cullen pushing his work

--- GeeYouEye.py
+++ GeeYouEye.py
@@ -1,20 +1,82 @@
 
 import tkinter
+import tkinter.messagebox
 from tkinter import *
 from PIL import ImageTk, Image
 import os
-
+from threading import Timer
+import time
 top = Tk()
-top.minsize(width=400, height=450)
-img = ImageTk.PhotoImage(Image.open("Test.png"))
-panel = Label(top, image = img)
-panel.pack(side = "bottom", fill = "both", expand = "yes")
 
+#initial allocation setup the 10 is hardcoded to allow up to 10 images
+img = [0 for i in range(10)]
+imgPanel = [0 for i in range(10)]
+
+#loading default image
+imgI = Image.open("default.png")
+imgI = ImageTk.PhotoImage(imgI)
+imgPanelI = Label(top, image = imgI)
+imgPanelI.image = imgI
+imgPanelI.grid(row=1, column=1, columnspan=10, rowspan=7)
+songBox = Listbox(top)
+songBox.grid(row=0, column=0, rowspan=7)
+for item in ["song_uno", "song_dos", "song_tres", "song_quatro", "song_cinco_de_mayo"]:
+    songBox.insert(END, item)
 
 #butt stuff, coding for buttons goes in corresponding functions below
 def qui():
     top.destroy()
+def confSong():
+    tkinter.messagebox.showinfo("Song Info", songBox.get(songBox.curselection()))
+    t1.start()
+    
+def timeout0():
+    #size needs to change based on amount of pictures
+    size = 4
+    looper = 0
+    while (looper < size):
+        img[looper] = Image.open("test" + str(looper + 1) + ".png")
+        img[looper] = img[looper].resize((200, 200), Image.ANTIALIAS)
+        img[looper] = ImageTk.PhotoImage(img[looper])
+        imgPanel[looper] = Label(top, image = img[looper])
+        imgPanel[looper].image = img[looper]
+        looper += 1
+    timeout1(0, size)
+#recursive function to display the pictures
+def timeout1(track, limit):
+    if (track != 0):
+        imgPanel[track - 1].grid_forget()
+    else:
+        imgPanelI.grid_forget()
+    imgPanel[track].grid(row=1, column=1, columnspan=10, rowspan=7)
+    time.sleep(3)#this will need to be changed based on (size)/(song lenth)
+    if (track < limit - 1):
+        timeout1(track + 1, limit)
+    else:
+        tkinter.messagebox.showinfo("END", "Song Complete")
+    
+    
+# Code to add widgets will go here...
+t1 = Timer(0, timeout0)
+buttConfirm = tkinter.Button (top, activebackground='green', text = "Confirm", command=confSong)
+buttEXIT = tkinter.Button (top, text = "X", command=qui)
+buttEXIT.grid(row=0, column=10)
+buttConfirm.grid(row=7, column=0)
+top.mainloop()
 
+
+'''
+img3 = Image.open("test2.png")
+img3 = img3.resize((200, 200), Image.ANTIALIAS)
+img4 = ImageTk.PhotoImage(img3)
+img = Image.open("test1.png")
+img = img.resize((200, 200), Image.ANTIALIAS)
+img = ImageTk.PhotoImage(img)
+panel1 = Label(top, image = img)
+panel2 = Label(top, image = img4)
+panel1.image = img
+panel2.image = img4
+panel1.grid(row=1, column=1, columnspan=10, rowspan=7)
 def song1():
     tkinter.messagebox.showinfo("Song Info", "Playing Song1")
 def song2():
@@ -24,18 +86,23 @@ def song3():
 def song4():
     tkinter.messagebox.showinfo("Song Info", "Playing Song4")
 def song5():
-    tkinter.messagebox.showinfo("8===SongInfo===D", "Playing Song5")
-# Code to add widgets will go here...
+    tkinter.messagebox.showinfo("8=SongInfo=D", "Playing Song5")
+buttSONG1.grid(row=0, column=0)
+buttSONG2.grid(row=1, column=0)
+buttSONG3.grid(row=2, column=0)
+buttSONG4.grid(row=0, column=1)
+buttSONG5.grid(row=1, column=1)
 buttSONG1 = tkinter.Button (top, activebackground='red', text = "Enter_Song_1_Here", command=song1)
 buttSONG2 = tkinter.Button (top, activebackground='blue', text = "Enter_Song_2_Here", command=song2)
 buttSONG3 = tkinter.Button (top, activebackground='green', text = "Enter_Song_3_Here", command=song3)
 buttSONG4 = tkinter.Button (top, activebackground='yellow', text = "Enter_Song_4_Here", command=song4)
 buttSONG5 = tkinter.Button (top, activebackground='pink', text = "Enter_Song_5_Here", command=song5)
-buttEXIT = tkinter.Button (top, text = "Exit", command=qui)
-buttSONG1.pack()
-buttSONG2.pack()
-buttSONG3.pack()
-buttSONG4.pack()
-buttSONG5.pack()
-buttEXIT.pack()
-top.mainloop()
+    if (track == 1):
+        panel1.grid_forget()
+        panel2.grid(row=1, column=1, columnspan=10, rowspan=7)
+        track = 0
+    else:
+        panel2.grid_forget()
+        panel1.grid(row=1, column=1, columnspan=10, rowspan=7)
+        track = 1
+'''
